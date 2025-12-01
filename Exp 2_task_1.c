c5c7d16431ae57ebba52611ebded24e5ef4cc02b
alialtamas418-glitch <alialtamas418@gmail.com>
2025-12-01 23:05:19 +0530

    Add files via upload

---
+++ Exp 2_task_1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+    /*
+    Name: Mohd Altamash Ali
+    CLASS: COMPS F.Y. DIVISION: C
+    UIN: 251P039
+    */
+    int n;
+    printf("Enter the Number:");
+    scanf("%d",&n);
+    if(n%2==0)
+    {
+        printf("Entered Number is Even...!");
+    }
+    else
+    {
+        printf("Entered Number is Odd");
+    }
+    return 0;
+}
